eadf625962194c55742bd555a0d7d86ca8628bbb
JosefinaOller <64923768+JosefinaOller@users.noreply.github.com>
2021-11-07 17:20:53 -0300

    Se agregó la implementación de la tabla Hash
    Hay que ver cómo vamos a hacer la distribución de la tabla hash en los trackers.

--- Sistema-Intercambio-Archivos/Nodo-Tracker/nodo_tracker.js
+++ Sistema-Intercambio-Archivos/Nodo-Tracker/nodo_tracker.js
@@ -5,7 +5,7 @@ const udp = require('dgram');
 const fs = require('fs');
 
 // Clase que representa a un Nodo Tracker (podríamos llegar a hacer algo así).
-const NodoTracker = function (id, ip, port, vecinos, es_primer_tracker){
+const NodoTracker = function (id, ip, port, vecinos, es_primer_tracker){ //Hay que ver como vamos a distribuir la tabla hash en los nodos
     
     this.id = id; // Los nodos trackers tienen un identificador único.
     this.ip = ip; // IP para recibir mensajes de otro nodo tracker o del servidor web (UDP).
@@ -13,10 +13,15 @@ const NodoTracker = function (id, ip, port, vecinos, es_primer_tracker){
     this.vecinos = vecinos; // Nodos trackers vecinos, es de la forma [{ip:'ip', port:'port'}, {ip:'ip', port:'port'}]
     this.es_primer_tracker = es_primer_tracker; // flag para identificar al primer nodo tracker de la "lista".
 
-    this.tabla_hash = {}; // estructura tipo diccionario {clave:valor}
+    this.tabla_hash = new HashTable(255); // estructura tipo diccionario {clave:valor}; puse 255 para probar
+
 
     this.agregar_archivo = function (hash, filename, filesize, pair_nodes){
-        this.tabla_hash[hash] = [filename, filesize, pair_nodes];
+        
+        this.tabla_hash.insert(hash,[filename,filesize,pair_nodes]);
+
+        //this.tabla_hash[hash] = [filename, filesize, pair_nodes];
+
         // 'pair_nodes' es una lista cuyos elementos son objetos del tipo {pairIP:'ip', pairPort:port}
         // que representan al socket TCP del nodo par que contiene parte del archivo en cuestión.
         // (asumo que será algo asi, hay que revisarlo..)
@@ -37,9 +42,9 @@ const NodoTracker = function (id, ip, port, vecinos, es_primer_tracker){
           const remotePort = info.port;
 
           console.log(`Mensaje recibido de [${remoteAddress}:${remotePort}]: ${msg}`);
-          console.log(`Respuesta desde tracker [${server.address().address}:${server.address().port}]: Gracias por tu mensaje!\n`);
+          console.log(`Respuesta desde tracker [${server.address().address}:${server.address().port}]: Alta del archivo confirmado!\n`);
           
-          server.send('Gracias por tu mensaje!', remotePort, remoteAddress); // For connectionless sockets, the destination port and address must be specified
+          server.send('Alta del archivo confirmado!', remotePort, remoteAddress); // For connectionless sockets, the destination port and address must be specified
         });
 
         server.on('listening', () => {
@@ -64,6 +69,39 @@ const NodoTracker = function (id, ip, port, vecinos, es_primer_tracker){
     
 }
 
+//Clase que representa a la estructura de tabla hash
+const HashTable = function (size_nodo){ //size es para especificar el tamaño en cada nodo. 
+    this.size = size_nodo;
+    this.buckets = Array(this.size);
+
+    for(let i=0; i < this.buckets.length ;i++){ //para instanciar Map[key,value]
+        this.buckets[i] = new Map(); 
+    } 
+
+    this.insert = function (key,value){
+        let i=Number.parseInt(key,16); //al hash lo parseo a decimal para el indice
+        this.buckets[i].set(key,value);
+    }
+
+    this.remove = function (key){
+        let i=Number.parseInt(key,16);
+        let deleted = this.buckets[i].get(key);
+        this.buckets[i].delete(key);
+        return deleted;
+    }
+
+    this.search = function(key){
+        let i=Number.parseInt(key,16);
+        return this.buckets[i].get(key);
+    }
+
+    this.toString = function(){ //Debe haber una mejor manera de iterar 
+        for(let i=0;i<this.buckets.length;i++){
+            console.log(this.buckets[i]);
+        }
+    }
+} 
+
 // Leemos y parseamos la configuración inicial de los nodos trackers (archivo JSON)
 let contenido_config = fs.readFileSync('config.json');
 let config_trackers = JSON.parse(contenido_config);
@@ -76,15 +114,23 @@ t3 = config_trackers[2];
 t4 = config_trackers[3];
 
 // Instanciamos los nodos trackers (definimos que serán 4)
-const nodo_tracker_1 = new NodoTracker(t1.id, t1.ip, t1.port, t1.vecinos, t1.es_primer_tracker);
-const nodo_tracker_2 = new NodoTracker(t2.id, t2.ip, t2.port, t2.vecinos, t2.es_primer_tracker);
-const nodo_tracker_3 = new NodoTracker(t3.id, t3.ip, t3.port, t3.vecinos, t3.es_primer_tracker);
-const nodo_tracker_4 = new NodoTracker(t4.id, t4.ip, t4.port, t4.vecinos, t4.es_primer_tracker);
+const nodo_tracker_1 = new NodoTracker(t1.id, t1.ip, t1.port, t1.vecinos, t1.es_primer_tracker); 
+const nodo_tracker_2 = new NodoTracker(t2.id, t2.ip, t2.port, t2.vecinos, t2.es_primer_tracker); 
+const nodo_tracker_3 = new NodoTracker(t3.id, t3.ip, t3.port, t3.vecinos, t3.es_primer_tracker); 
+const nodo_tracker_4 = new NodoTracker(t4.id, t4.ip, t4.port, t4.vecinos, t4.es_primer_tracker); 
 
 console.log(nodo_tracker_1.toString());
 console.log(nodo_tracker_2.toString());
 console.log(nodo_tracker_3.toString());
 console.log(nodo_tracker_4.toString());
+
+//PROBANDO LA IMPPLEMENTACION HASH
+
+nodo_tracker_1.agregar_archivo('3a','Sims',60,['1',20]); //probando, funciona, ok
+if(nodo_tracker_1.tabla_hash.search('3a')){ //para ver si esta el key '3a', funciona ok
+    console.log('True');
+}
+nodo_tracker_1.tabla_hash.toString();
 
 // Cada nodo tracker comienza a escuchar en su puerto UDP
 nodo_tracker_1.escuchar_UDP();
@@ -94,6 +140,8 @@ nodo_tracker_4.escuchar_UDP();
 // [esto de que los 4 trackers escuchen a la vez (en distintos puertos) sin quedarnos bloqueados en el primero,
 // es posible gracias a que los métodos de UDP son asincronicos (actuan como hilos de ejecucion distintos al 'main')
 // igual hay que preguntar si para el tp podemos hacerlo así, o si hay que abrir cada tracker en una consola distinta].
+
+
 
 /*
 // ---------pruebas-instancias-nodos-tracker-------------
