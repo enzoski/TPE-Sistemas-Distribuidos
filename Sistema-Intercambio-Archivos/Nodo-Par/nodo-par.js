c226e0bca70da1083313f194c9179b94c4ccc2b3
valenfernandez <valenfernandez@users.noreply.github.com>
2021-12-04 23:56:50 -0300

    Completado nodo par con busqueda de archivo en el directorio y descarga de archivo solicitado a partir del hash
    -además cambie la forma de lectura de los archivos, usando 'fs' que es mas sencillo.

--- Sistema-Intercambio-Archivos/Nodo-Par/nodo-par.js
+++ Sistema-Intercambio-Archivos/Nodo-Par/nodo-par.js
@@ -4,27 +4,34 @@ const my_port = 4001;  // deberiamos tener un archivo de configuracion como en l
 
 // terminal par: descargar archivo.torrente
 const prompt = require('prompt-sync')();
-const file = prompt("archivo torrente:");
+const file = prompt("archivo torrente:"); //file hola.torrente 
+
+
 // abro archivo paso a JSON y extraigo hash, ip y puerto del tracker
-var reader = new FileReader();
-reader.readAsText(file);
-reader.onload = function() {
-    const torrente = JSON.parse(reader.result);
+
+let fs = require('fs');
+ fs.readFile(file, 'utf-8', (err,data) => {
+     if (err){
+         console.log(err);
+         return;
+     }
+    const torrente = JSON.parse(data);
     let hash = torrente['hash'];
     let ip_tracker = torrente['ip_tracker'];
     let port_tracker = torrente['port_tracker'];
     solicitar_descarga(hash, ip_tracker, port_tracker);
-  };
-reader.onerror = function() {
-    console.log(reader.error);
-  };
+ });
+
 
 // comunicacion con trackers
     // Solicitud archivo torrente
-    function solicitar_descarga(hash, ip_tracker, port_tracker){ 
+    function solicitar_descarga(hash, ip_tracker, port_tracker){
+        let pares;
+        let nombre_archivo;
+
         let search = {
-            messageId: '123', 
-            route: `/file/{${hash}}`,
+            messageId: '123', // ???
+            route: `/file/${hash}`,
             originIP: my_IP,
             originPort: my_port
         } ;
@@ -32,39 +39,44 @@ reader.onerror = function() {
         const client = udp.createSocket('udp4');
         client.send(JSON.stringify(search), port_tracker, ip_tracker);
         client.on('message', function (msg) { 
-            let respuesta = JSON.parse(msg.toString());
-            const pares = respuesta['body']['pares']; //aca tengo el arreglo de {}
+            let respuesta = JSON.parse(msg.toString()); //llega un found
+            pares = respuesta['body']['pares']; //aca tengo el arreglo de pares: [{parip, },{}]
+            nombre_archivo = respuesta['filename'];
             client.close();
         });
- 
-        const ip_par =pares[0]['parIP'] ; // esto no anda no se como acceder al elemento!!!
-        const port_par = pares[0]['parPort'];
-        descargar(hash,ip_par,port_par);
+
+        //ACA SIEMPRE ACCEDO AL PRIMERO, PODRIAMOS HACER QUE SI ESA DESCARGA ME DEVUELVE UN ERROR INTENTE CON EL SEGUNDO
+
+        let ip_par = pares[0]['parIP'] ;
+        let port_par = pares[0]['parPort'];
+        descargar(hash,ip_par,port_par, nombre_archivo);
     }
     
 
 // comunicacion con otros pares
     //Solicitud de descarga de archivo (abrir un socket, consultar el archivo y empezar a pasar bytes)
 
-    function descargar(hash, ip_par, port_par){
+    function descargar(hash, ip_par, port_par, nombre_archivo){
+        const fs = require('fs');
+        const net = require('net');
+        const client = new net.Socket();
         let solicitud = JSON.stringify({
             type: 'GET FILE',
             hash: hash
         });
-        const net = require('net');
-        const client = new net.Socket();
 
         client.connect(port_par, ip_par, function() {
             client.write(solicitud);
         });
 
         client.on('data', function(data) {
-            console.log('Received: ' + data); // se recibio el stream de bytes
-            // convertir el stream de bytes a un file??
+            fs.writeFile(nombre_archivo, data, (err) => {
+            if (err)
+                console.log(err);
+            });
             client.end();
         });
     }
-
 // TCP server
 // server.js
 
@@ -77,44 +89,48 @@ server.listen(my_port, function () { // no se si escucha en ese puerto
 
 function conexionEntrante (socket) {
 
+    const fs = require('fs');
+
 	socket.on('data', function(data) {
 		let solicitud = JSON.parse(data.toString());
-        let hash = solicitud['hash'];
-        //busco mi archivo que tenga ese mismo hash. ->lista de archivos? carpeta archivos compartidos? 
-		socket.write('archivo');
+        let hash = solicitud['hash']; //hash buscado
+        let hash_actual;
+        let respuesta;
+
+        //parte del codigo sacado de https://www.codegrepper.com/code-examples/javascript/javascript+foreach+file+in+directory
+        
+        const path = require('path');
+        const fs = require('fs');
+        const directoryPath = path.join(__dirname, 'archivos');
+        fs.readdir(directoryPath, function (err, files) {
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            } 
+            files.forEach(function (file) {
+                let filename = file;
+                var stats = fs.statSync(`./archivos/${file}`);
+                let filesize = stats.size; //en bytes
+                hash_actual = sha1(filename+filesize);
+                if (hash_actual = hash){
+                    
+                    fs.readFile(`./archivos/${filename}`, 'utf-8', (err,data) => {
+                        if (err){
+                            console.log(err);
+                            return;
+                        }
+                        respuesta = data;
+                    });
+                }
+            });
+            // aca se puede agregar un if !respuesta ... enviar un archivo no encontrado o algo asi.
+        });
+
+		socket.write(respuesta); //mando el contenido del archivo.
 	});
 
     server.close();
-
 }
 
-// Respondo al tracker si tengo un archivo
-const udp = require('dgram');
-const server = udp.createSocket('udp4');
 
-server.on('error', (err) => {
-    console.log(`Error:\n${err.stack}`);
-    server.close();
-});
-
-server.on('message', (msg,info) => {
-
-    const remoteAddress = info.address;
-    const remotePort = info.port;
-
-    //console.log(`Mensaje recibido de [${remoteAddress}:${remotePort}]: ${msg}`);
     
-    let solicitud = JSON.parse(msg.toString()); 
-    //busco en mis archivos si existe el que me estan pidiendo? tengo una carpeta de archivos compartidos en cada par? o mantengo una lista de archivos compartidos?
-
-    server.send('respuesta', remotePort, remoteAddress); 
-});
-
-server.on('listening', () => {
-    const address = server.address(); // Returns an object containing the address information for a socket
-    console.log(`Servidor UDP escuchando en ${address.address}:${address.port} ...\n`);
-});
-
-// For UDP sockets, 'bind()' causes the dgram.Socket to listen for datagram messages on a named port and optional address.
-// If address is not specified, the operating system will attempt to listen on all addresses (all network interfaces of the computer).
-server.bind(this.port, this.ip);
+    
