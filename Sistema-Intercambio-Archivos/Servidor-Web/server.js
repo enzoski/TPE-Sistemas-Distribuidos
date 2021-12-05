491f18c098c052efbff44bf557b8f23e6f016c59
Enzo Barria <74929931+enzoski@users.noreply.github.com>
2021-12-05 15:26:25 -0300

    Servidor Web: Implementada la comunicación UDP con el nodo tracker
    * Ante una petición HTTP GET /file (listar archivos), el servidor le envía un 'scan' al primer nodo tracker de la red, respondiendo con la lista de todos los archivos de la red disponibles para descargar.
    * Ante una petición HTTP GET /file/<hash> (descargar archivo .torrente), el servidor le envía un 'search' al primer nodo tracker de la red, respondiendo con la información necesaria para generar, o no (en caso de no encontrarse el archivo solicitado), el .torrente.
    * Ante una petición HTTP POST /file/ (alta de archivos), el servidor le envía un 'store' al primer nodo tracker de la red, respondiendo con la confirmación del éxito o no, de la carga de los datos del nuevo archivo en algún nodo tracker (y así el nuevo archivo podrá ser ubicado en la red).

--- Sistema-Intercambio-Archivos/Servidor-Web/server.js
+++ Sistema-Intercambio-Archivos/Servidor-Web/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const IP_server = 'localhost';
+const port_server = 8080;
 //const bodyParser = require('body-parser');
 const cors = require('cors');
 //const path = require('path');
 const sha1 = require('sha1'); //para el hash
-var id; // id de los torrentes que se van subiendo. No es 0, es el hash del archivo
-const IP_tracker = 'localhost'; // IP y PUERTO del primer nodo tracker de la "lista".
+const udp = require('dgram'); //para la comunicación UDP con el primer nodo tracker de la red.
+// IP y PUERTO del primer nodo tracker de la "lista" de trackers.
+const IP_tracker = 'localhost';
 const port_tracker = 3001;
-const udp = require('dgram');
-const IP_server = 'localhost';
 
 // No hace falta usar 'bodyParser.urlencoded', porque eso nos sirve para decodificar el 'body' de un POST realizado por un formulario HTML,
 // y en nuestro caso vamos a usar el 'fetch' para hacer los POST, cuyo 'body' es un JSON --> entonces usamos: express.json().
@@ -19,26 +19,12 @@ const IP_server = 'localhost';
 app.use(express.json()); // Para que el servidor sepa como decodificar el 'body' que viene en un POST en formato JSON.
 app.use(cors()); // Para que el servidor pueda responder sin inconvenientes las peticiones HTTP provenientes de un 'fetch'.
 
+// ******************************* GESTIÓN DE LAS PETICIONES HTTP *******************************
+
 app.get('/', function(req,res){ // PAGINA DE INICIO
-	mostrar_info_peticion(req);
-	//res.sendFile('index.html',{root: path.join(__dirname,'./public')});
-	res.sendFile('index.html', {root: 'public'});
-});
-
-app.get('/file', function(req,res){ // LISTAR ARCHIVOS 
-	mostrar_info_peticion(req);
-    // En principio no haría falta devolver el HTML completo del index, solo el bloque que contenga el listado.
-	// res.sendFile('index.html',{root: path.join(__dirname,'./public')});
-	// res.sendFile('index.html', {root: 'public'});
-    let respuesta = listar_archivos();
-    //res.send('Lista nombres y tamaños de archivos'); //para probar
-    res.send(respuesta); // le mandamos al cliente el 'scan' como viene, en un string JSON.
-});
-
-app.get('/file/hash', function(req,res){ // SOLICITUD DE DESCARGA
-	mostrar_info_peticion(req);
-	solicitud_descarga(); //voy a la funcion de solicitud descarga
-    res.send('Torrente descargado con éxito!'); //para probar
+    mostrar_info_peticion(req);
+    //res.sendFile('index.html',{root: path.join(__dirname,'./public')});
+    res.sendFile('index.html', {root: 'public'});
 });
 
 app.get('/formulario', function(req,res){ // Para ir al formulario de alta de archivos
@@ -47,121 +33,192 @@ app.get('/formulario', function(req,res){ // Para ir al formulario de alta de ar
     res.sendFile('formulario_alta_torrentes.html', {root: 'public'});
 });
 
-app.get('/peticiones_cliente.js', function(req,res){ // Para que al entrar a la pagina del forumalrio, el navegador reciba el .js
+app.get('/peticiones_cliente.js', function(req,res){ // Para que al entrar a la pagina del formulario, el navegador reciba el .js
     mostrar_info_peticion(req);
     res.sendFile('peticiones_cliente.js', {root: 'public'});
 });
 
+app.get('/file', function(req,res){ // LISTAR ARCHIVOS 
+    mostrar_info_peticion(req);
+    listar_archivos(res); // le pasamos el 'res' para que haga ahi dentro el 'res.send(respuesta)', ya que el metodo que recibe un mensaje UDP, es asíncrono.
+});
+
+app.get('/file/:hash', function(req,res){ // SOLICITUD DE DESCARGA .TORRENTE
+    mostrar_info_peticion(req);
+    // El formato '/file/:hash' indica que 'hash' actua como un parametro, algo variable,
+    // ya que el hash de un archivo es justamente distinto al de otro, y por ende los GET tendrán rutas distintas.
+    let hash_archivo = req.params.hash; // y de esta manera obtenemos el valor del parametro. https://expressjs.com/es/4x/api.html#req.params
+    solicitud_descarga(hash_archivo, res);
+});
+
 app.post('/file/', function(req,res){ // ALTA DE ARCHIVOS (la peticion viene de un 'fetch' del cliente)
-	
-	mostrar_info_peticion(req);
+    
+    mostrar_info_peticion(req);
     console.log(req.body); // para pruebas
 
-    // parseamos el JSON recibido en el 'body' del POST (son los datos del torrente a subir).
+    // Parseamos el JSON recibido en el 'body' del POST (son los datos del nuevo archivo
+    // que que será parte de la red, y que trackeará un nodo tracker).
+    //let datos_archivo = JSON.parse(req.body);
     // --> NO HACE FALTA, express ya lo convierte a un objeto javascript por nosotros [gracias al "app.use(express.json());"].
-    //let datos_torrente = JSON.parse(req.body);
-    let datos_torrente = req.body;
+    let datos_archivo = req.body; // https://expressjs.com/es/api.html#req.body
 
-	// obtenemos el id para el nuevo torrente utilizando la función hash
-    id = obtener_hash(datos_torrente.filename,datos_torrente.filesize);
-    
-    //id = id + 1; no es contador, es el hash del archivo
+    // Obtenemos el id para el nuevo archivo utilizando la función hash (el id de un archivo es el hash SHA-1).
+    id = obtener_hash(datos_archivo.filename, datos_archivo.filesize);
 
-    // procesamos el alta del nuevo torrente
-	alta_archivo(id, datos_torrente.filename, datos_torrente.filesize, datos_torrente.nodeIP, datos_torrente.nodePort);
+    // Procesamos el alta del nuevo archivo, es decir, se cargará en algun nodo tracker, los datos para ubicar al nuevo archivo en la red.
+    alta_archivo(id, datos_archivo.filename, datos_archivo.filesize, datos_archivo.nodeIP, datos_archivo.nodePort, res);
 
-    // cuando tocas el boton de SUBIR, queda la misma pagina por si el cliente desea subir más de un archivo
-    // res.sendFile('formulario-client.html',{root: path.join(__dirname,'./public')});
-    // En principio no haría falta volver a mandarle el HTML del formulario, luego de tocar el boton ya queda en esa pagina.
-
-    res.send(`Torrente "${datos_torrente.filename}" subido con éxito!`); // Le respondemos al 'fetch' proveniente del cliente (navegador).
+    // Nota: cuando en el HTML se toca el boton de SUBIR, queda la misma pagina por si el cliente desea subir más de un archivo.
 });
 
 
 // El servidor empieza a escuchar en el puerto que corresponda
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto: ${port}...`);
+app.listen(port_server, IP_server, () => {
+  console.log(`Servidor escuchando en el puerto: ${port_server}...`);
 });
 
 
+// ******************* FUNCIONES PARA LA COMUNICACIÓN UDP CON EL NODO TRACKER *******************
+//                             Y POSTERIOR RESPUESTA AL CLIENTE WEB
 
-
-function alta_archivo(id, filename, filesize, nodeIP, nodePort){
-    
-    let hash = obtener_hash(filename,filesize);
-    let store = {
-        messageId: '1', //???????? hay que hacerlo incremental o que onda?
-        route: `/file/{${hash}}/store`,
-        originIP: IP_server,
-        originPort: port_tracker,
-        body: {
-            id: '', //?????????????
-            filename: filename,
-            filesize: filesize,
-            parIP: nodeIP,
-            parPort: nodePort
-        }
-    };
-
-    const client = udp.createSocket('udp4');
-    client.send(JSON.stringify(store), port_tracker, IP_tracker);
-    client.on('message', function (msg) { 
-        console.log('Received: ' + msg.toString());
-        client.close();
-    });
-}
-
-function listar_archivos(){
+function listar_archivos(res){
+    // Armamos el mensaje 'scan' para enviarle al primer nodo tracker de la red.
     let scan = {
-        messageId: '2', //??????????? hay que hacerlo incremental o que onda?
+        messageId: '', // En principio, solo el primer nodo tracker gestiona este atributo.
         route: '/scan',
         originIP: IP_server,
-        originPort: port, // si falla algo, revisar este puerto.
-        body: undefined // por lo que dice la interfaz
+        originPort: port_server, // si falla algo, revisar este puerto.
+        body: undefined // se deja 'undefined' por lo que dice la interfaz. El primer nodo tracker se encargará de armar la estructura.
     };
+    // Abrimos un socket UDP cliente para comunicarnos con el nodo tracker.
     const client = udp.createSocket('udp4');
     client.send(JSON.stringify(scan), port_tracker, IP_tracker);
-    client.on('message', function (msg) { //quizas el tener abierto varios servers udp en el mismo puerto traiga problemas si hacemos solicitudes concurrentes.
-        //const respuesta = JSON.parse(msg);
-        console.log('Received: ' + msg); //mientras tanto, asumimos que las solicitudes se hacen una por ves.
+    // El nodo tracker nos responderá con la lista de archivos de toda la red (es el formato de un 'scan').
+    client.on('message', function (msg) {
+        //quizas el tener abierto varios servers udp en el mismo puerto traiga problemas si hacemos solicitudes concurrentes.
+        //mientras tanto, asumimos que las solicitudes se hacen una por vez.
         
+        mostrar_info_respuesta('Respuesta del Tracker ante Petición GET /file', msg);
+        
+        // Le respondemos al cliente web tal cual lo que nos respondió el nodo tracker, que será un string JSON (con el formato del 'scan' pero con el '.body' rellenado).
+        // Luego el cliente web se encargará de hacer el JSON.parse(msg) para obtener los datos de los archivos e insertarlos adecuadamente en el HTML.
+        res.send(msg);
         client.close();
-        return msg;
     });
-
-    console.log('Lista nombres y tamaños de archivos');
+    // Por si hay un error con el socket en sí.
+    client.on('error', (err) => {
+        console.log(`Error:\n${err.stack}`);
+        client.close();
+    });
+    // Al ejecutarse el método 'client.on('message')', como es asincrono, no nos quedaremos bloqueados
+    // esperando la respuesta del tracker, sino que cuando responda, se ejecutará el callback pasado por parametro.
+    // Entonces cuando el servidor llama a esta funcion 'listar_archivos()',
+    // rapidamente termina de ejecutarse al llegar a esta linea de código, y le vuelve el control.
 }
 
-function solicitud_descarga(hash){ 
+function solicitud_descarga(hash, res){
+    // Armamos el mensaje 'search' para enviarle al primer nodo tracker de la red.
     let search = {
-        messageId: '3', //??????????? hay que hacerlo incremental o que onda?
-        route: `/file/{${hash}}`,
+        messageId: '', // En principio, solo el primer nodo tracker gestiona este atributo.
+        route: `/file/${hash}`,
         originIP: IP_server,
-        originPort: port_server
-    } ;
-
+        originPort: port_server,
+        body: {} // por lo que dice la interfaz, queda vacio (solo está para que el formato de mensaje sea similar a otros mensajes).
+    };
+    // Abrimos un socket UDP cliente para comunicarnos con el nodo tracker.
     const client = udp.createSocket('udp4');
     client.send(JSON.stringify(search), port_tracker, IP_tracker);
+    // El nodo tracker nos responderá con la información necesaria del archivo solicitado para generar su .torrente (es el formato de un 'found').
+    // En caso de no encontrar el archivo en la red, de igual forma recibiremos un 'found', pero con su '.body' vacío ({}).
     client.on('message', function (msg) { 
-        console.log('Received: ' + msg.toString());
+        mostrar_info_respuesta(`Respuesta del Tracker ante Petición GET /file/${hash}`, msg);
+        // Convertimos el string JSON recibido a un objeto JavaScript.
+        msg = JSON.parse(msg);
+        // Verificamos si se encontró o no el archivo solicitado.
+        let respuesta;
+        if(Object.keys(msg.body).length == 0){
+            // https://expressjs.com/es/api.html#res.status
+            res.status(404); // seteamos el codigo de estado HTTP a 404 (Not Found), así del lado del cliente podremos comprobar si hubo un error o no.
+            respuesta = JSON.stringify({error: 'ERROR: Archivo no encontrado.'});
+        }
+        else{
+            // Preparamos el Header de la respuesta HTTP para que el navegador directamente descargue el .torrente.
+            // http://expressjs.com/en/api.html#res.set
+            res.set({
+              'Content-Disposition': `attachment; filename="${msg.body.filename.split('.')[0]}.torrente"`,
+              'Content-Type': 'text/plain'
+            });
+            // Preparamos el contenido del .torrente que se descargará.
+            let contenido_torrente = {
+                hash: msg.body.id,
+                trackerIP: msg.body.trackerIP,
+                trackerPort: msg.body.trackerPort
+            };
+            // Convertimos el contenido del .torrente a un string JSON y se lo enviamos al cliente web (navegador) para que automaticamente lo descargue.
+            respuesta = JSON.stringify(contenido_torrente); // la respuesta al cliente web es el contenido del archivo .torrente que se generará.
+        }
+
+        res.send(respuesta);
         client.close();
     });
-
-    console.log('Solicitud de descarga solicitada');
-    /*GET /file/{hash}
-Content-Disposition: attachment; filename=”nombre.torrente”
-Content-Type: text/plain
-*/
+    // Por si hay un error con el socket en sí.
+    client.on('error', (err) => {
+        console.log(`Error:\n${err.stack}`);
+        client.close();
+    });
 }
 
+function alta_archivo(id, filename, filesize, nodeIP, nodePort, res){
+    // Armamos el mensaje 'store' para enviarle al primer nodo tracker de la red.
+    let store = {
+        messageId: '', // En principio, solo el primer nodo tracker gestiona este atributo.
+        route: `/file/${id}/store`, // El id es el hash del archivo.
+        originIP: IP_server,
+        originPort: port_server,
+        body: {
+            id: id,
+            filename: filename,
+            filesize: filesize,
+            pares: [{
+                parIP: nodeIP, // Por tema de compatibilidad, se puso los datos del par en un arreglo, pero siempre habrá un solo par.
+                parPort: nodePort
+            }]
+        }
+    };
+    // Abrimos un socket UDP cliente para comunicarnos con el nodo tracker.
+    const client = udp.createSocket('udp4');
+    client.send(JSON.stringify(store), port_tracker, IP_tracker);
+    // El nodo tracker nos responderá con un mensaje informando el éxito o no de la carga del nuevo archivo (es un objeto con 3 atributos).
+    client.on('message', function (msg) { 
+        mostrar_info_respuesta('Respuesta del Tracker ante Petición POST /file/', msg);
+        // Le respondemos al cliente web tal cual lo que nos respondió el nodo tracker, que será un string JSON.
+        // Luego el cliente web se encargará de hacer el JSON.parse(msg) para obtener la confirmacion de la carga y mostrar un mensaje en el HTML.
+        res.send(msg); // Le respondemos al 'fetch' proveniente del cliente (navegador).
+        client.close();
+    });
+    // Por si hay un error con el socket en sí.
+    client.on('error', (err) => {
+        console.log(`Error:\n${err.stack}`);
+        client.close();
+    });
+}
+
+// ********************************* FUNCIONES COMPLEMENTARIAS **********************************
 
 function mostrar_info_peticion(req){
     // Información sobre la petición del cliente
     // https://nodejs.org/api/http.html#http_class_http_incomingmessage
     const metodo_peticion_cliente = req.method;
     const path_peticion_cliente = req.url;
-    console.log(`[${Date().split(' ')[4]}]`); // Hora actual del sistema
-    console.log(`Petición entrante: ${metodo_peticion_cliente} ${path_peticion_cliente}\n`);
+    console.log(`\n[${Date().split(' ')[4]}]`); // Hora actual del sistema
+    console.log(`Petición entrante: ${metodo_peticion_cliente} ${path_peticion_cliente}`);
+}
+
+function mostrar_info_respuesta(texto, respuesta){
+    // Información sobre la respuesta del nodo tracker
+    console.log(`\n[${Date().split(' ')[4]}]`); // Hora actual del sistema
+    console.log(texto);
+    console.log(JSON.parse(respuesta)); // la respuesta viene en crudo (string JSON)
 }
 
 function obtener_hash(filename,filesize){ //PRODUCE HASH PERO HAY QUE VER COMO HACER PARA EVITAR COLISIONES
