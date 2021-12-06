d035f9629c624bef29fb67a329b9b7fd8a7eccc4
Enzo Barria <74929931+enzoski@users.noreply.github.com>
2021-12-05 23:22:17 -0300

    Cliente Web: gestionada la respuesta de peticiones 'Listar archivos'
    Cuando el cliente web realiza un 'fetch()' al servidor web con la petición HTTP GET /file, el servidor le responde con un arreglo que contiene la información de todos los archivos de la red. Los elementos de dicho arreglo se insertan adecuadamente en la pagina HTML mediante una tabla dinámica (cada fila representa a un archivo).

--- Sistema-Intercambio-Archivos/Servidor-Web/public/peticiones_cliente.js
+++ Sistema-Intercambio-Archivos/Servidor-Web/public/peticiones_cliente.js
@@ -1,8 +1,6 @@
 // No necesitamos hacer un "require('node-fetch')", ya que el navegador ya tiene integrado un 'fetch'.
 // De hecho, a priori tampoco podríamos importar ese modulo porque es propio de npm/node (el interprete de JS del navegador no lo reconocería).
 
-const { json } = require("body-parser");
-
 // El 'fetch' nos permite desde el navegador, hacerle peticiones HTTP al servidor web con código javascript,
 // para que nos devuelva un recurso concreto y podamos gestionar desde acá (.js),
 // por ejemplo, cómo insertarlo en el HTML actual para que se vea en el navegador.
@@ -17,40 +15,13 @@ https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Using_Fetch
 
 function listar_archivos(){
   fetch('http://localhost:8080/file') // interfaz para acceder y manipular partes de http peticiones y respuestas
-    .then(response => response.text()) // en principio, el servidor nos mandaría el listado de archivos en formato HTML, y lo insertaríamos en el index.html.
-    .then(data => alert(data)); // para probar, se puso un alert
-    
-    //var listadata = JSON.stringify(data);
-	var datalista = [
-      {
-          "filename": "jueguito1",
-          "filesize": "1",
-          "nodeIP": "223",
-          "nodePort": "224",
-          hash: ""
-      },
-      {
-          "filename": "jueguito2",
-          "filesize": "1",
-          "nodeIP": "223",
-          "nodePort": "224",
-          hash: ""
-      },
-      {
-          "filename": "jueguito3",
-          "filesize": "1",
-          "nodeIP": "223",
-          "nodePort": "224",
-          hash: ""
-      }
-    ]
-    return datalista;
-    //return listadata; 
+    .then(response => response.json()) // el servidor nos responde con el listado de archivos en formato JSON, y lo insertaríamos en el index.html.
+    .then(data => crear_tabla_dinamica(data)); // en 'data' nos quedará el string JSON parseado a objeto JavaScript, el cual será de la forma: [{id: hash1, filename: nombre1, filesize: tamaño1}, ...]
 }
 
-function solicitud_descarga(){
-  fetch('http://localhost:8080/file/hash')
-    .then(response => response.text())
+function solicitud_descarga(hash){
+  fetch(`http://localhost:8080/file/${hash}`)
+    .then(response => response.json())
     .then(data => alert(data)); // para probar, se puso un alert
 }
 
@@ -77,7 +48,7 @@ function alta_archivo(){
         },
       body: JSON.stringify(datos_torrente) // convertimos el objeto de JS en string JSON, que es lo que enviaremos.
     })
-    .then(response => response.text()) // el servidor nos responde un string de confirmación.
+    .then(response => response.json()) // el servidor nos responde un string de confirmación.
     .then(data => alert(data)); // mostramos el mensaje en el navegador con una alerta.
 
   // Limpiamos los campos del formulario
@@ -86,5 +57,68 @@ function alta_archivo(){
   document.getElementById('filesize').value = "";
   document.getElementById('nodeIP').value = "";
   document.getElementById('nodePort').value = "";
+
+}
+
+
+function crear_tabla_dinamica(lista_archivos){
+
+   var col = [];
+   for (var i = 0; i < lista_archivos.length; i++) {
+       for (var key in lista_archivos[i]) {
+           if (col.indexOf(key) === -1) {
+               col.push(key);
+           }
+       }
+   }
+
+  col.push("boton");
+
+  // CREATE DYNAMIC TABLE.
+  var table = document.createElement("table");
+
+  // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+   var tr = table.insertRow(-1);                   // TABLE ROW = fila.
+
+   for (var i = 0; i < col.length; i++) {
+       var th = document.createElement("th");      // TABLE HEADER.
+       th.innerHTML = col[i];   //sirve para asignar dato
+       tr.appendChild(th);
+   }
+
+  // ADD JSON DATA TO THE TABLE AS ROWS.
+   for (var i = 0; i < lista_archivos.length; i++) {
+
+       tr = table.insertRow(-1);
+
+       var hash_archivo;
+
+       for (var j = 0; j < col.length-1; j++) {
+           var tabCell = tr.insertCell(-1);
+           tabCell.innerHTML = lista_archivos[i][col[j]];
+           console.log(lista_archivos[i][col[j]]);
+           if (j == 0)
+            hash_archivo = lista_archivos[i][col[j]]; // resguardamos el valor del hash para luego agregarlo al boton de descarga.
+       }
+      //Create an input type dynamically.   
+      var buttond = document.createElement("input");
+      //Assign different attributes to the element. 
+      buttond.setAttribute("type", "button");
+      buttond.setAttribute("value", "descargar");
+      buttond.setAttribute("name", "descargar");
+      //buttond.setAttribute("onclick", solicitud_descarga());
+      console.log(hash_archivo);
+      buttond.onclick = function () {
+          solicitud_descarga(hash_archivo);
+      }
+      var tabCell = tr.insertCell(-1);
+      tabCell.append(buttond);
+   }
+
+   // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+   var divContainer = document.getElementById("showData");
+   divContainer.innerHTML = "";
+   divContainer.appendChild(table);
 
 }
